1defdd63fa28b3b8e05ceecee72ee8342e39b511
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-27 16:09:23 +0100

    impostati html e css, inizio js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
+// Rifate (con immagini a piacere e con il vostro gusto grafico) lo slider di immagini in jQuery come l'abbiamo visto stamattina.
+// Aggiungete anche i pallini che si "muovono" di pari passo con le immagini.
+// BONUS: cliccando su un pallino, si attiva l'immagine corrispondente. Ad esempio: sto visualizzando la prima immagine, clicco sul quarto pallino e si attiva direttamente la quarta immagine (oltre al pallino corrispondente ovviamente!)
 
+$(document).ready(function() {
+// attivare al click la freccia sx
+    $ (".arrow-left i").click(function() {
+        // alert("click");
+
+    })
+
+    $ (".arrow-right i").click(function() {
+        // alert("click");
+
+    })
 });
